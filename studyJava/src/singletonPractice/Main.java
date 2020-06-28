826cb636125158daf692450fe553c5ca692b5b01
yhh1056 <yhh1056@naver.com>
2020-06-28 16:37:03 +0900

    싱글 톤 패턴

---
+++ studyJava/src/singletonPractice/Main.java
@@ -0,0 +1,13 @@
+package singletonPractice;
+
+/**
+ * 클레스 설명
+ * <p>
+ * author {yhh1056}
+ * Create by {2020/06/28}
+ */
+public class Main {
+    public static void main(String[] args) {
+        StudentWhoIsTakeExam.getStudding();
+    }
+}
