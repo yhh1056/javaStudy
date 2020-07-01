ce0fba4e563987576424c9404250c706c29f2d73
yhh1056 <yhh1056@naver.com>
2020-07-01 22:32:18 +0900

    템플릿 메서드

---
+++ studyJava/src/templetMehodPra/Tier.java
@@ -0,0 +1,13 @@
+package templetMehodPra;
+
+/**
+ * 클레스 설명
+ * <p>
+ * author {yhh1056}
+ * Create by {2020/07/01}
+ */
+public abstract class Tier {
+    public abstract void drive();
+    public abstract void drift();
+    public abstract void power();
+}
