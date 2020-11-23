db99dccc20750cc28ae645ec8a72b1d0561b67dd
yhh1056 <yhh1056@naver.com>
2020-11-23 23:22:55 +0900

    메모리 구조

---
+++ studyJava/src/memory/Main.java
@@ -0,0 +1,11 @@
+package memory;
+
+/**
+ * https://www.notion.so/9a32318f7a3448b9a54e153db9826edd
+ * 주소에 메모리 과정에 대한 설명이 담겨있다.
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello JAVA");
+	}
+}
